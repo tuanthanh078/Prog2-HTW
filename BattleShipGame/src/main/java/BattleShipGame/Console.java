1983ab433581626d6e8fd61c0977067b3a3abd33
Tuan Thanh Tran <tuanthanh078@gmail.com>
2020-09-06 18:50:19 +0200

    Generate documentation

--- BattleShipGame/src/main/java/BattleShipGame/Console.java
+++ BattleShipGame/src/main/java/BattleShipGame/Console.java
@@ -1,35 +1,70 @@
 package BattleShipGame;
 
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * This class implements methods for reading integers or strings
+ * from System.in (stdin)
+ */
 
 public class Console {
 
     private transient BufferedReader reader = null;
-
+    
+    /**
+     * Specific constructor for dependency injection.
+     * 
+     * @param reader    BufferReader for manipulations of inputs.
+     */
     public Console(BufferedReader reader) {
         this.reader = reader;
     }
 
+    /**
+     * Default constructor for class.
+     */
     public Console() {
         this.reader = new BufferedReader(new InputStreamReader(System.in));
     }
 
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message   Message for the user.
+     * @return Valid integer.
+     */
     public int readIntegerFromStdin(String message) {
         return readIntegerFromStdin(message, "Is not a valid number!");
     }
     
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message   Message for the user.
+     * @return Valid string.
+     */
     public String readStringFromStdin(String message) {
         return readStringFromStdin(message, "Is not a valid string!");
     }
     
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message   Message for the user.
+     * @return Valid binary number.
+     */
     public int readBinaryFromStdin(String message) {
         return readBinaryFromStdin(message, "Is not a valid number!");
     }
     
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message       Message for the user.
+     * @param errorMessage  Error message for the user.
+     * @return Valid integer.
+     */
     public int readIntegerFromStdin(String message, String errorMessage) {
         do {
             try {
@@ -41,6 +76,13 @@ public class Console {
         } while (true);
     }
 
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message       Message for the user.
+     * @param errorMessage  Error message for the user.
+     * @return Valid string.
+     */
     public String readStringFromStdin(String message, String errorMessage) {
         do {
             try {
@@ -52,6 +94,13 @@ public class Console {
         } while (true);
     }
     
+    /**
+     * Returns a valid input from command line interface (CLI).
+     * 
+     * @param message       Message for the user.
+     * @param errorMessage  Error message for the user.
+     * @return Valid binary number.
+     */
     public int readBinaryFromStdin(String message, String errorMessage) {
         do {
             try {
@@ -65,13 +114,24 @@ public class Console {
             }
         } while (true);
     }
-
+    
+    /**
+     * Prints out a message for the user
+     * 
+     * @param message   Message for the user.
+     */
     private void writeMessageToStdout(String message) {
         System.out.print(message);
     }
 
+    /**
+     * Prints out an error message for the user
+     * 
+     * @param e             Exception
+     * @param errorMessage  Error message for the user.
+     */
     private void writeErrorToStdErr(Exception e, String errorMessage) {
         System.err.println(errorMessage == null ? e.getMessage() : errorMessage);
-    }
-}
+    } 
 
+}
