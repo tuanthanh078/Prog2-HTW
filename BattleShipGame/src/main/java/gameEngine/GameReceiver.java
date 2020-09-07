bdf054e843e56873b209540941822077dc4789e5
Tuan Thanh Tran <tuanthanh078@gmail.com>
2020-09-07 12:21:53 +0200

    Generate documentation

--- BattleShipGame/src/main/java/gameEngine/GameReceiver.java
+++ BattleShipGame/src/main/java/gameEngine/GameReceiver.java
@@ -1,11 +1,39 @@
 package gameEngine;
 
-
-
 import java.io.IOException;
 
+/**
+ * This class is an interface for a receiver of game data.
+ *
+ */
 public interface GameReceiver {
+    
+    /**
+     * Receives dice number
+     * 
+     * @param random    dice number
+     * @throws IOException
+     * @throws StatusException
+     */
     void receiveDice(int random) throws IOException, StatusException;
+    
+    /**
+     * Receives coordinates of the point to shoot
+     * 
+     * @param x     coordinate x of the point
+     * @param y     coordinate y of the point
+     * @throws IOException
+     * @throws StatusException
+     * @throws GameException
+     */
     void receiveShoot(int x, int y) throws IOException, StatusException, GameException;
+    
+    /**
+     * Receives result after being shot
+     * 
+     * @param result    description of result
+     * @throws IOException
+     * @throws StatusException
+     */
     void receiveShootResult(int[] result) throws IOException, StatusException;
 }
--- BattleShipGame/src/main/java/gameEngine/GameSender.java
+++ BattleShipGame/src/main/java/gameEngine/GameSender.java
@@ -1,11 +1,39 @@
 package gameEngine;
 
-
-
 import java.io.IOException;
 
+/**
+ * This class is an interface for a sender of game data.
+ *
+ */
 public interface GameSender {
+    
+    /**
+     * Sends dice number
+     * 
+     * @param random    dice number
+     * @throws IOException
+     * @throws StatusException
+     */
     void sendDice(int random) throws IOException, StatusException;
+    
+    /**
+     * Sends coordinates of the point to shoot
+     * 
+     * @param x     coordinate x of the point
+     * @param y     coordinate y of the point
+     * @throws IOException
+     * @throws StatusException
+     * @throws GameException
+     */
     void sendShoot(int x, int y) throws IOException, StatusException, GameException;
+    
+    /**
+     * Sends result after being shot
+     * 
+     * @param result    description of result
+     * @throws IOException
+     * @throws StatusException
+     */
     void sendShootResult(int[] result) throws IOException, StatusException;
 }
